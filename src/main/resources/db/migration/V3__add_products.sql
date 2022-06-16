5efa068e87a540c1933f741f2cd9af693bd588b2
ValdemarTaik <95375825+ValdemarTaik@users.noreply.github.com>
2022-06-16 15:43:48 +0300

    Add files via upload

--- src/main/resources/db/migration/V3__add_products.sql
+++ src/main/resources/db/migration/V3__add_products.sql
@@ -1,16 +1,16 @@
 INSERT INTO products (id, title, price)
-values (1, 'Cheese', 450.0);
+values (1, 'Protein Belarus', 45.0);
 
 INSERT INTO products (id, title, price)
-values (2, 'Beer', 45.0);
+values (2, 'Creatin', 150.0);
 
 INSERT INTO products (id, title, price)
-values (3, 'Milk', 65.0);
+values (3, 'Protein Poland', 65.0);
 
 INSERT INTO products (id, title, price)
-values (4, 'Tomato', 115.0);
+values (4, 'BCA Ukraine', 200.0);
 
 INSERT INTO products (id, title, price)
-values (5, 'Bread', 58.0);
+values (5, 'BCA Germany', 58.0);
 
 ALTER SEQUENCE product_seq RESTART WITH 6;
