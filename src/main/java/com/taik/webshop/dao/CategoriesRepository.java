fc45c7511af74a18a0456ec6af47752ba6dde299
ValdemarTaik <95375825+ValdemarTaik@users.noreply.github.com>
2022-06-21 08:56:04 +0300

    Delete CategoriesRepository.java

--- src/main/java/com/taik/webshop/dao/CategoriesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taik.webshop.dao;
-
-import com.taik.webshop.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriesRepository extends JpaRepository<Category, Long> {
-}
